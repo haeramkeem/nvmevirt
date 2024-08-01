ab6f63da7a2afcf17bbf47695ceaa9d336e95122
haeramkeem <haeram.kim1@gmail.com>
2024-08-01 01:08:22 +0000

    feat: config KVS

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MEMMAP_START=7
-MEMMAP_SIZE=1
+MEMMAP_START=$1
+MEMMAP_SIZE=$2
 
-sudo modprobe nvmev memmap_start=${MEMMAP_START}G memmap_size=${MEMMAP_SIZE}G cpus=2,3
+sudo modprobe nvmev memmap_start=${MEMMAP_START}G memmap_size=${MEMMAP_SIZE}G cpus=0,1
